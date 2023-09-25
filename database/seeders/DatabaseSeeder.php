be0ed6fcb32cac31ae5a3a430a7014a65139a20b
Preedarat Chuthong <preedarat.ch@Preedarats-MacBook-Pro.local>
2023-09-25 23:15:33 +0700

    adjust role, permission table and seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\RoleHasPermission;
+use App\Models\User;
+use App\Models\UserHasRole;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +18,65 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $roleData = array('Admin', 'Viewer', 'Editor');
+        foreach ($roleData as $role) {
+            $role = Role::create(['name' => $role]);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $permissionData = array('Create', 'Read', 'Update', 'Delete');
+        foreach ($permissionData as $permission) {
+            $permission = Permission::create(['name' => $permission]);
+        }
+
+        // assign permissions to roles
+        $mapping = array(
+            'Admin' => array('create', 'read', 'update', 'delete'),
+            'Viewer' => array('read'),
+            'Editor' => array('create', 'read', 'update'),
+        );
+        foreach ($mapping as $role => $permissions) {
+            $role = Role::where('name', $role)->first();
+
+            foreach ($permissions as $permission) {
+                $permission = Permission::where('name', $permission)->first();
+                RoleHasPermission::create([
+                    'role_id' => $role->id,
+                    'permission_id' => $permission->id,
+                ]);
+            }
+        }
+        $userData = array(
+            array(
+                'name' => fake()->name(),
+                'email' => 'admin@email.com',
+                'password' => 'password',
+                'role' => 'Admin'
+            ),
+            array(
+                'name' => fake()->name(),
+                'email' => 'viewer@email.com',
+                'password' => 'password',
+                'role' => 'Viewer'
+            ),
+            array(
+                'name' => fake()->name(),
+                'email' => 'editor@email.com',
+                'password' => 'password',
+                'role' => 'Editor'
+            ),
+        );
+        // create users
+        foreach ($userData as $value) {
+            $user = User::create([
+                'name' => $value['name'],
+                'email' => $value['email'],
+                'password' => $value['password'],
+            ]);
+            $role = Role::where('name', $value['role'])->first();
+            UserHasRole::create([
+                'user_id' => $user->id,
+                'role_id' => $role->id,
+            ]);
+        }
     }
 }
